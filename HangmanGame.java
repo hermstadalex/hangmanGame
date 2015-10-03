190cb50b884503176452a150678c42f0caece670
Alexander Hermstad <hermstadalex@gmail.com>
2015-10-03 15:35:42 -0700

    Finished functionality for one-word phrases

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Character;
+import java.util.Random;
 
 
 public class HangmanGame{
@@ -11,6 +12,9 @@ public class HangmanGame{
   private String wordProgress;
   private char currentGuess;
   private int currNumIndicies = 0;
+  private int numLettersGuessed = 0;
+  private int correctGuesses = 0;
+  private boolean letterGuessed = false;
 
 
   HangmanGame() {
@@ -33,6 +37,10 @@ public class HangmanGame{
     return wordProgress;
   }
 
+  public int getCurrNumIndicies(){
+    return currNumIndicies;
+  }
+
   public int getStageInGame(){
     return stageInGame;
   }
@@ -40,6 +48,12 @@ public class HangmanGame{
   public void setCurrentWord(String word){
     if(word.length() < PHRASE_LENGTH_MAX){
       this.currentWord = word;
+    }
+  }
+
+  public void setStageInGame(int stage){
+    if (stage >= 0 && stage <= 6) {
+      stageInGame = stage;
     }
   }
   
@@ -58,8 +72,24 @@ public class HangmanGame{
     Scanner reader = new Scanner(System.in);
     char userGuess = reader.next().charAt(0);
     if (Character.isLetter(userGuess)){
-      currentGuess = Character.toUpperCase(userGuess);
-      return true;
+      if (numLettersGuessed == 0){
+        currentGuess = Character.toUpperCase(userGuess);
+        lettersGuessed[0] = currentGuess;
+        numLettersGuessed += 1;
+        return true;
+      }
+
+      else {
+        for(int i = 0; i < numLettersGuessed; i++) {
+          if (lettersGuessed[i] == Character.toUpperCase(userGuess) ) {
+            return false;
+          }
+        }
+        currentGuess = Character.toUpperCase(userGuess);
+        lettersGuessed[numLettersGuessed] = currentGuess;
+        numLettersGuessed += 1;
+        return true;
+      }
     }
     else {
       return false;
@@ -76,24 +106,126 @@ public class HangmanGame{
       }
     }
     currNumIndicies = numIndicies;
+    if (numIndicies == 0) {
+      stageInGame += 1;
+    }
+    correctGuesses += currNumIndicies;
     return letterLocationIndicies;
   }
 
-  private int
-
   public void updateWordProgress(int[] letterLocationIndicies){
-    String newWordProgress = ""
-    if(letterLocationIndicies[0] != null){                     //Use Substring
-      for(int i = 0; i < currNumIndicies; i++)
+    String newWordProgress = wordProgress;
+    if(currNumIndicies > 0){                     //Use Substring
+      for(int i = 0; i < currNumIndicies; i++){
         if(letterLocationIndicies[i] == 0){
-          
+          newWordProgress = currentGuess + wordProgress.substring(1);
+          wordProgress = newWordProgress;
         }
+        else if(letterLocationIndicies[i] == (currentWord.length() - 1) ) {
+          newWordProgress = wordProgress.substring(0, wordProgress.length() - 1)
+          + currentGuess;
+          wordProgress = newWordProgress;
+        }
+        else{
+          newWordProgress = wordProgress.substring(0,
+              2*letterLocationIndicies[i]) + currentGuess +
+            wordProgress.substring(2 * letterLocationIndicies[i] + 1);
+          wordProgress = newWordProgress;
+        }
+      }
+    }
+    wordProgress = newWordProgress;
+    currNumIndicies = 0;
+  }
+
+  public void showWordProgress() {
+    System.out.println(wordProgress);
+  }
+
+  public void showFigure() {
+    switch (stageInGame) {
+      case 0: 
+        break;
+      case 1:
+        System.out.println("          *");
+        break;
+      case 2:
+        System.out.println("          *");
+        System.out.println("          |");
+        break;
+      case 3:
+        System.out.println("          *");
+        System.out.println("         -|");
+        break;
+      case 4:
+        System.out.println("          *");
+        System.out.println("         -|-");
+        break;
+      case 5:
+        System.out.println("          *");
+        System.out.println("         -|-");
+        System.out.println("         /  ");
+        break;
+      case 6:
+        System.out.println("          *");
+        System.out.println("         -|-");
+        System.out.println("         / \\");
+        break;
     }
 
   }
 
-  public static void main( String[] args){
-    HangmanGame firstGame = new HangmanGame("longtest");
-    System.out.println(firstGame.getWordProgress());
+  public void showLettersUsed() {
+    System.out.println("Letters Used: ");
+    for (int i = 0; i < numLettersGuessed; i++) {
+      if (i == (numLettersGuessed - 1) ) {
+        System.out.print(lettersGuessed[i]);
+      }
+      else {
+        System.out.print(lettersGuessed[i] + ", ");
+      }
+    }
   }
+
+  public void displayUI() {
+    System.out.println("\n\n\n");
+    this.showFigure();
+    System.out.println("\n\n");
+    this.showWordProgress();
+    System.out.println("\n\n");
+    this.showLettersUsed();
+    System.out.println("\n\nPlease enter your guess: \n\n ");
+  }
+
+  public void gameRunner() {
+    displayUI();
+    while (true) {
+      if(this.getUserLetter()) {
+        this.updateWordProgress(this.checkLetter());
+      }
+      else {
+        System.out.println("Not a valid letter, or already guessed. Try again");
+      }
+      displayUI();
+      if ( correctGuesses == currentWord.length() ) {
+        System.out.println("\nCongragulations, you win! \n");
+        break;
+      }
+      if ( stageInGame == 6 ) {
+        System.out.println("\nSorry, you lose! \n");
+        break;
+      }
+    }
+  }
+
+  public static void main( String[] args){
+    final String[] hangmanTestWords = {"keyboard", "mouse", "computer", "fantastic",
+                                  "excellent", "eraser", "mechanical"};
+    int numWords = 7;
+    Random wordSelector = new Random();
+    int wordIndex = wordSelector.nextInt(numWords);
+    HangmanGame testGame = new HangmanGame(hangmanTestWords[wordIndex]);
+    testGame.gameRunner();
+    }
+  
 }
